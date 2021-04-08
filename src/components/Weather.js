954e44acb24b588735d4157aa2cafd7e61844c21
Paul Shivers <Paul.S.Shivers@gmail.com>
2021-04-08 16:39:09 -0400

    got weather api hooked up

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,30 +1,59 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 function Weather() {
+  const [weather, setWeather] = useState(null);
+  const [lon, setLon] = useState(null);
+  const [lat, setLat] = useState(null);
 
-    const [weather, setWeather] = useState({conditions: "Sunny"});
+  function success(res) {
+    setLon(res.coords.longitude);
+    setLat(res.coords.latitude);
+    // getWeather();
+  }
+  function error(err) {
+    alert(err);
+  }
 
-    function getWeather() {        
-        // setWeather('Rainy');
-        const lon = 113.17;
-        const lat = 23.09;
-        fetch(`http://www.7timer.info/bin/api.pl?lon=${lon}&lat=${lat}&product=civil&output=json`)
-        .then(res=>res.json())
-        .then(body=>{
-            console.log(body)
-        });
-    }
+  function handleLocationClick() {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
 
-    getWeather();
+  function getWeather() {
+    fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${
+        process.env.REACT_APP_WEATHER_API_KEY
+      }&q=${"Atlanta"}&days=1&aqi=no&alerts=no`
+    )
+      .then((res) => res.json())
+      .then((body) => {
+        setWeather(res.body.current);
+        console.log(body);
+      });
+  }
 
+  function getLocation() {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+  getLocation();
+
+  if (!lon || !lat) {
     return (
-      <div className="Weather weather-wrapper">        
-          Weather 
-        <div>
-            {weather.conditions}
-        </div>  
+      <div className='Weather'>
+        <button onClick={handleLocationClick}>Click to share location</button>
+        Must share location in order to enable weather
+      </div>
+    );
+  } else {
+    return (
+      <div className='Weather weather-wrapper'>
+        <button onClick={getWeather}>Get Weather</button>
+        <div>longitude: {lon}</div>
+        <div>latitude: {lat}</div>
+        Weather
       </div>
     );
   }
-  
-  export default Weather;
+}
+
+export default Weather;
